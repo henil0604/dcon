514aa4cb54b0997e87a5921669c447af5dc4770a
Henil Malaviya <henilm.dev@gmail.com>
2024-05-07 21:19:28 +0530

    feat: setup google provider

--- src/lib/server/auth/index.ts
+++ src/lib/server/auth/index.ts
@@ -1,5 +1,27 @@
+import { AUTH_GOOGLE_CLIENT_ID, AUTH_GOOGLE_CLIENT_SECRET } from '$env/static/private';
 import { SvelteKitAuth } from '@auth/sveltekit';
+import GoogleProvider from '@auth/sveltekit/providers/google';
+
+const GOOGLE_SCOPES = [
+	'openid',
+	'https://www.googleapis.com/auth/userinfo.email',
+	'https://www.googleapis.com/auth/userinfo.profile',
+	'https://www.googleapis.com/auth/drive'
+];
 
 export const { handle: authHandle } = SvelteKitAuth({
-	providers: []
+	providers: [
+		GoogleProvider({
+			clientId: AUTH_GOOGLE_CLIENT_ID,
+			clientSecret: AUTH_GOOGLE_CLIENT_SECRET,
+			authorization: {
+				params: {
+					prompt: 'consent',
+					access_type: 'offline',
+					response_type: 'code',
+					scope: GOOGLE_SCOPES.join(' ')
+				}
+			}
+		})
+	]
 });
