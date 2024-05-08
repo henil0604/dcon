c85fe9338409fa4766a8521d2c2342d112ded2d5
Henil Malaviya <henilm.dev@gmail.com>
2024-05-08 16:51:16 +0530

    chore: basic demo of FileSystemRenderer

--- src/routes/drive/+page.svelte
+++ src/routes/drive/+page.svelte
@@ -1,1 +1,18 @@
-<div>hi</div>
+<script>
+	import FileSystemRenderer from '$lib/components/FileSystemRenderer.svelte';
+</script>
+
+<FileSystemRenderer
+	entities={[
+		{
+			name: 'Untitled Folder',
+			id: '123',
+			type: 'DIR'
+		},
+		{
+			name: 'Hello World',
+			id: '456',
+			type: 'FILE'
+		}
+	]}
+/>
