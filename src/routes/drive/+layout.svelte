d515907032fdae047e074e8cbba8b3be471ee217
Henil Malaviya <henilm.dev@gmail.com>
2024-05-08 16:14:28 +0530

    ui: added very basic static topbar

--- src/routes/drive/+layout.svelte
+++ src/routes/drive/+layout.svelte
@@ -1,8 +1,37 @@
 <script lang="ts">
+	import { page } from '$app/stores';
+	import Breadcrumb from '$lib/components/Breadcrumb.svelte';
 	import AppLayout from '$lib/components/layout/AppLayout.svelte';
+	import { Button } from '$lib/components/ui/button';
+	import { Plus } from 'lucide-svelte';
 </script>
 
 <AppLayout>
 	<!-- body -->
-	<slot />
+	<div class="bodyWrapper">
+		<!-- top bar -->
+		<div class="flex">
+			<!-- breadcrumb -->
+			<div class="flex-grow">
+				<Breadcrumb />
+			</div>
+
+			<!-- tail -->
+			<div class="flex">
+				<Button class="h-fit p-2" size="sm"><Plus size={18} /></Button>
+			</div>
+		</div>
+
+		<hr class="my-4" />
+
+		<slot />
+	</div>
 </AppLayout>
+
+<style lang="scss">
+	.bodyWrapper {
+		display: flex;
+		flex-direction: column;
+		padding: theme('padding.6') theme('padding.6');
+	}
+</style>
