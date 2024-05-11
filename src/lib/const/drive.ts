3ff9838945e34c0c53d200b8f017f176d2944203
Henil Malaviya <henilm.dev@gmail.com>
2024-05-11 15:41:57 +0530

    chore (refactor): using bytes package

--- src/lib/const/drive.ts
+++ src/lib/const/drive.ts
@@ -1,10 +1,12 @@
+import bytes from 'bytes';
+
 export const DRIVE_UPLOAD_DIRECTORY_NAME = '_DCON';
 
-export const DEFAULT_MAX_CHUNK_SIZE = 1024 * 1024 * 1; // 1 MB
+export const DEFAULT_MAX_CHUNK_SIZE = bytes('1 MB');
 
-export const DEFAULT_STREAM_CHUNK_SIZE = 1024 * 1; // 1 KB
+export const DEFAULT_STREAM_CHUNK_SIZE = bytes('1 KB');
 
-export const MIN_CHUNK_SIZE = 1024 * 100; // 100 KB
+export const MIN_CHUNK_SIZE = bytes('100 KB');
 
 export const CONCURRENT_CHUNK_UPLOADING = 3;
 
