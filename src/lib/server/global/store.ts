2020a50c00e87210b08f9618370c9000964243cf
Henil Malaviya <henilm.dev@gmail.com>
2024-05-11 15:35:21 +0530

    chore: set store as global entity

--- src/lib/server/global/store.ts
+++ src/lib/server/global/store.ts
@@ -3,3 +3,4 @@ import { Store } from '$lib/server/modules/store';
 const globalForStore = globalThis as unknown as { store: Store };
 
 export const store = globalForStore.store || new Store();
+globalForStore.store = store;
