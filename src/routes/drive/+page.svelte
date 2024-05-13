d94fc5b45552779ab467078b04d4c29b96138355
Henil Malaviya <henilm.dev@gmail.com>
2024-05-13 12:12:30 +0530

    feat: very minimal (untested) file handling from dropzone

--- src/routes/drive/+page.svelte
+++ src/routes/drive/+page.svelte
@@ -1,72 +1,47 @@
 <script lang="ts">
 	import FileSystemRenderer from '$lib/components/FileSystemRenderer.svelte';
-	import { trpc } from '$lib/trpc/client';
-	import { onMount } from 'svelte';
-	import { fetchEventSource } from '@microsoft/fetch-event-source';
+	import { dropzone } from '$lib/utils/frontend/dropzone';
+	import DropzoneOverlayPopup from '$lib/components/DropzoneOverlayPopup.svelte';
+	import { handleFileUpload } from '$lib/utils/frontend/upload';
 
-	async function test() {
-		const form = new FormData();
-		let data = new Array(Math.ceil(1024 * 1024 * 0.1)).fill('A').join('');
-		form.set('file', new File([data], 'hello.txt'));
-
-		let localFileUploadResponse = await fetch('/api/upload', {
-			body: form,
-			method: 'POST'
-		});
-
-		const fileId = (await localFileUploadResponse.json()).data.id;
-
-		console.log(fileId);
-
-		const response = trpc().drive.upload.mutate({
-			fileId,
-			fileInfo: {
-				name: 'hello.txt',
-				mimeType: 'text/plain'
-			}
-		});
-
-		response.then(console.log);
-
-		const source = await fetchEventSource(`/api/upload/progress?fileId=${fileId}`, {
-			async onopen(response) {
-				console.log(response);
-			},
-			onmessage(event) {
-				console.log(JSON.parse(event.data));
-			},
-			onclose() {
-				console.log('closed');
-			},
-			onerror(err) {
-				console.log('ERR', err);
-			},
-			method: 'GET',
-			headers: {
-				Accept: 'text/event-stream'
-			},
-			openWhenHidden: true
-		});
-
-		console.log(source);
-	}
-
-	onMount(() => {
-		test();
-	});
+	let showFileDropzoneOverlay = false;
 </script>
 
-<FileSystemRenderer
-	entities={[
-		{
-			name: 'Untitled Folder',
-			id: '123',
-			type: 'DIR'
+<div
+	class="relative h-full w-full"
+	use:dropzone={{
+		onDraggingChange(isDragging) {
+			showFileDropzoneOverlay = isDragging;
 		},
-		{
-			name: 'Hello World',
-			id: '456',
-			type: 'FILE'
+		handleFiles: (files) => {
+			if (files.length === 0) {
+				return;
+			}
+
+			let filesArray = Array.from(files);
+
+			for (const file of filesArray) {
+				const factory = handleFileUpload(file);
+				factory.start();
+			}
 		}
-	]}
-/>
+	}}
+>
+	{#if showFileDropzoneOverlay}
+		<DropzoneOverlayPopup />
+	{/if}
+	<FileSystemRenderer
+		entities={[
+			{
+				name: 'Untitled Folder',
+				id: '123',
+				type: 'DIR'
+			},
+			{
+				name: 'Hello World',
+				id: '456',
+				type: 'FILE'
+			}
+		]}
+	/>
+</div>
